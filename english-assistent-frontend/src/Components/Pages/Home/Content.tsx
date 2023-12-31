8d603409146dc1d32f12d04a023f9905877e9780
Myslitel666 <tretyakov_02@bk.ru>
2023-12-31 11:23:50 +0500

    The content has been moved to a separate file

---
+++ english-assistent-frontend/src/Components/Pages/Home/Content.tsx
@@ -0,0 +1,19 @@
+import Box from '@mui/material/Box';
+
+const Content: React.FC = () =>
+{ 
+    return (
+        <>
+            <Box bgcolor='#460000' width='50%'
+                minHeight='100vh' style={{ float: 'left' }}
+            >
+
+            </Box>
+            <Box bgcolor='#4E003D' width='50%'
+                minHeight='100vh' style={{ float: 'left', }}>
+            </Box>
+        </>
+    );
+}
+
+export default Content;
--- english-assistent-frontend/src/Components/Pages/Home/Home.tsx
+++ english-assistent-frontend/src/Components/Pages/Home/Home.tsx
@@ -1,20 +1,14 @@
 ﻿import React from 'react';
-import Box from '@mui/material/Box';
 
 //MyComponents Import
 import Header from '../../Common/Header/Header';
+import Content from '../Home/Content';
 
 const Home: React.FC = () => {
     return (
         <div>
             <Header />
-            <Box bgcolor='#FF1A1A' width='50%'
-                minHeight='100vh' style={{ float: 'left' }}
-            >
-            </Box>
-            <Box bgcolor='#FA14FF' width='50%'
-                minHeight='100vh' style={{ float: 'left', }}>
-            </Box>
+            <Content/>
         </div>
     );
 };
