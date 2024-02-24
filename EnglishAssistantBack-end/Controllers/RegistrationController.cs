791ef3f92eb027bbc2cc2fc7275b649a4da49625
Myslitel666 <tretyakov_02@bk.ru>
2024-02-24 16:09:57 +0500

    The account service is called in the registration controller.

--- EnglishAssistantBack-end/Controllers/RegistrationController.cs
+++ EnglishAssistantBack-end/Controllers/RegistrationController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using EnglishAssistantBackend.DTOs;
 using EnglishAssistantBackend.Repositories;
 using EnglishAssistantBackend.Interfaces.Repositories;
+using EnglishAssistantBackend.Interfaces.Services;
 
 namespace EnglishAssistantBackend.Controllers
 {
@@ -12,96 +13,19 @@ namespace EnglishAssistantBackend.Controllers
     [ApiController]
     public class RegistrationController : Controller
     {
-        private readonly IRoleRepository _roleRepository;
-        private readonly IUserRepository _userRepository;
+        private readonly IAccountService _accountService;
 
-        public RegistrationController(IUserRepository userRepository, IRoleRepository roleRepository)
+        public RegistrationController(IAccountService accountService)
         {
-            _roleRepository = roleRepository ?? throw new ArgumentNullException(nameof(roleRepository));
-            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _accountService = accountService ?? throw new ArgumentNullException(nameof(accountService));
         }
 
         [HttpPost("setUser")]
         public async Task<IActionResult> SetUser([FromBody] UserDto userDto)
         {
-            //Извлекаем пользователя из списка по username (в случае его отсутствия получим null)
-            var existingUser = await _userRepository.GetUserByUsername(userDto.Username);
+            var authorizationResponse = await _accountService.RegisterUser(userDto);
 
-            //Если пользователь с данным username уже присутствует в System
-            if (existingUser != null)
-            {
-                AuthorizationResponseDto response = new AuthorizationResponseDto()
-                {
-                    IsError = true,
-                    FeedbackMessage = "✗A user with this username already exists"
-                };
-                return Ok(response);
-            }
-
-            //Извлекаем пользователя из списка по паролю (в случае его отсутствия получим null)
-            existingUser = await _userRepository.GetUserByPassword(userDto.Password);
-
-            //Если пользователь с данным паролем уже присутствует в System
-            if (existingUser != null)
-            {
-                AuthorizationResponseDto response = new AuthorizationResponseDto()
-                {
-                    IsError = true,
-                    FeedbackMessage = "✗This password is already taken"
-                };
-                return Ok(response);
-            }
-
-            try
-            {
-                //Получаем идентификатор роли по её имени из базы данных
-                var role = await _roleRepository.GetIdByRole(userDto.Role);
-
-                if (role == null)
-                {
-                    return BadRequest(new
-                    {
-                        IsError = true,
-                        FeedbackMessage = "✗Role does not exist"
-                    });
-                }
-
-                var user = new User
-                {
-                    RoleId = role.RoleId,
-                    Username = userDto.Username,
-                    Password = userDto.Password,
-                };
-                await _userRepository.AddUser(user);
-
-                //Извлекаем пользователя из списка по username для отправки на client
-                var registeredUser = await _userRepository.GetUserByUsername(userDto.Username);
-
-                //Создаю экземпляр UserDto
-                UserDto userResponseDto = new UserDto()
-                {
-                    UserId = registeredUser.UserId,
-                    Role = role.RoleName,
-                    Username = registeredUser.Username,
-                };
-
-                AuthorizationResponseDto response = new AuthorizationResponseDto()
-                {
-                    IsError = false,
-                    FeedbackMessage = "✓User successfully registered",
-                    User = userResponseDto
-                };
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                AuthorizationResponseDto response = new AuthorizationResponseDto()
-                {
-                    IsError = true,
-                    FeedbackMessage = $"✗Failed to complete the registration. Error: {ex.Message}"
-                };
-                return BadRequest(response);
-            }
+            return Ok(authorizationResponse);
         }
     }
 }
